fdc7226530fa14be278618fae8cc2c9477759bd1
tommy <tommy19901015@gmail.com>
2022-08-28 15:57:29 +0800

    modify url

--- src/component/graphicsLine/main.jsx
+++ src/component/graphicsLine/main.jsx
@@ -1,33 +1,34 @@
-import React, { useState, useEffect } from 'react'
-import './css.scss'
+import React, { useState, useEffect } from "react";
+import "./css.scss";
 
 const GraphicsLine = () => {
-    return (
-        <div className="graphicsLineBlock">
-            <img
-                className="imgBlock"
-                src="https://webdev.hannstar.com/upload/home_templast_list_s/twL_home_template_20F05_z4wzanj6ws.jpg"
-                alt="成為夥伴"
-            />
-            <div className="contentBlock">
-                <div className="borderStyle">
-                    <div className="titleBlock">
-                        <div className="line"></div>
-                        <h2 className="title">成為夥伴</h2>
-                    </div>
-                    <div className="textBlock">
-                        加入彩晶，我們的用心您能看的見!彩晶致力為世界帶來更鮮艷的色彩、更精采的生活互動，同時我們更加重視內部菁英人才的發展與成長，實踐誠信、鼓勵創新、客戶導向、團隊合作的核心價值。
-                    </div>
-                    <div className="bottomLineBox">
-                        <div className="line"></div>
-                        <a className="learnMore" href="/careers-detail/join-hannstar/">
-                            LEARN MORE<span className="plusLine"></span>
-                        </a>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="graphicsLineBlock">
+      <img
+        className="imgBlock"
+        //   src="https://webdev.hannstar.com/upload/home_templast_list_s/twL_home_template_20F05_z4wzanj6ws.jpg"
+        src="https://fakeimg.pl/740x385/?text=1"
+        alt="成為夥伴"
+      />
+      <div className="contentBlock">
+        <div className="borderStyle">
+          <div className="titleBlock">
+            <div className="line"></div>
+            <h2 className="title">成為夥伴</h2>
+          </div>
+          <div className="textBlock">
+            加入彩晶，我們的用心您能看的見!彩晶致力為世界帶來更鮮艷的色彩、更精采的生活互動，同時我們更加重視內部菁英人才的發展與成長，實踐誠信、鼓勵創新、客戶導向、團隊合作的核心價值。
+          </div>
+          <div className="bottomLineBox">
+            <div className="line"></div>
+            <a className="learnMore" href="/careers-detail/join-hannstar/">
+              LEARN MORE<span className="plusLine"></span>
+            </a>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default GraphicsLine
+export default GraphicsLine;
--- src/component/messageCenter/main.jsx
+++ src/component/messageCenter/main.jsx
@@ -1,96 +1,102 @@
-import React, { useState, useEffect } from 'react'
-import './css.scss'
-import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
+import React, { useState, useEffect } from "react";
+import "./css.scss";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const messageCenterData = [
-    {
-        data: [
-            {
-                tag: '企業永續',
-                date: '2022.03.22',
-                title: '瀚宇彩晶加入「TALENT, In Taiwan，台灣人才永續行動聯盟」">瀚宇彩晶加入「TALENT, In Taiwan，台灣人才永續行動聯盟」',
-            },
-            {
-                tag: '企業永續',
-                date: '2022.03.11',
-                title: '38婦女節，多愛妳一點">38婦女節，多愛妳一點',
-            },
-            {
-                tag: '投資人關係',
-                date: '2022.03.09',
-                title: '瀚宇彩晶2022年2月份營收報告">瀚宇彩晶2022年2月份營收報告',
-            },
-        ],
-    },
-    {
-        data: [
-            {
-                tag: '投資人關係',
-                date: '2022.02.24',
-                title: '瀚宇彩晶2021 / 4Q營業報告新聞稿">瀚宇彩晶2021 / 4Q營業報告新聞稿',
-            },
-            {
-                tag: '投資人關係',
-                date: '2022.02.03',
-                title: '沒有人該成為孤島 為防疫英雄加油">沒有人該成為孤島 為防疫英雄加油',
-            },
-            {
-                tag: '投資人關係',
-                date: '2022.02.24',
-                title: '瀚宇彩晶2021 / 4Q營業報告新聞稿">瀚宇彩晶2021 / 4Q營業報告新聞稿',
-            },
-        ],
-    },
-    {
-        data: [
-            {
-                tag: '投資人關係',
-                date: '2022.02.24',
-                title: '瀚宇彩晶2021 / 4Q營業報告新聞稿">瀚宇彩晶2021 / 4Q營業報告新聞稿',
-            },
-            {
-                tag: '投資人關係',
-                date: '2022.02.03',
-                title: '沒有人該成為孤島 為防疫英雄加油">沒有人該成為孤島 為防疫英雄加油',
-            },
-        ],
-    },
-]
+  {
+    data: [
+      {
+        tag: "企業永續",
+        date: "2022.03.22",
+        title:
+          '瀚宇彩晶加入「TALENT, In Taiwan，台灣人才永續行動聯盟」">瀚宇彩晶加入「TALENT, In Taiwan，台灣人才永續行動聯盟」',
+      },
+      {
+        tag: "企業永續",
+        date: "2022.03.11",
+        title: '38婦女節，多愛妳一點">38婦女節，多愛妳一點',
+      },
+      {
+        tag: "投資人關係",
+        date: "2022.03.09",
+        title: '瀚宇彩晶2022年2月份營收報告">瀚宇彩晶2022年2月份營收報告',
+      },
+    ],
+  },
+  {
+    data: [
+      {
+        tag: "投資人關係",
+        date: "2022.02.24",
+        title:
+          '瀚宇彩晶2021 / 4Q營業報告新聞稿">瀚宇彩晶2021 / 4Q營業報告新聞稿',
+      },
+      {
+        tag: "投資人關係",
+        date: "2022.02.03",
+        title:
+          '沒有人該成為孤島 為防疫英雄加油">沒有人該成為孤島 為防疫英雄加油',
+      },
+      {
+        tag: "投資人關係",
+        date: "2022.02.24",
+        title:
+          '瀚宇彩晶2021 / 4Q營業報告新聞稿">瀚宇彩晶2021 / 4Q營業報告新聞稿',
+      },
+    ],
+  },
+  {
+    data: [
+      {
+        tag: "投資人關係",
+        date: "2022.02.24",
+        title:
+          '瀚宇彩晶2021 / 4Q營業報告新聞稿">瀚宇彩晶2021 / 4Q營業報告新聞稿',
+      },
+      {
+        tag: "投資人關係",
+        date: "2022.02.03",
+        title:
+          '沒有人該成為孤島 為防疫英雄加油">沒有人該成為孤島 為防疫英雄加油',
+      },
+    ],
+  },
+];
 
 const MessageCenter = (porps) => {
-    const settings = {
-        dots: false,
-        arrows: true,
-        fade: false,
-        infinite: true,
-        speed: 1000,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        // className: 'bannerSlider',
-        // dotsClass: 'bannerDots',
-    }
+  const settings = {
+    dots: false,
+    arrows: true,
+    fade: false,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // className: 'bannerSlider',
+    // dotsClass: 'bannerDots',
+  };
 
-    return (
-        <div className="messageCenter">
-            <Slider {...settings}>
-                {messageCenterData.map((item, idx) => (
-                    <div>
-                        {item.data.map((data) => {
-                            return (
-                                <li>
-                                    <div>{data.tag}</div>
-                                    <div>{data.date}</div>
-                                    <div>{data.title}</div>
-                                </li>
-                            )
-                        })}
-                    </div>
-                ))}
-            </Slider>
-        </div>
-    )
-}
+  return (
+    <div className="messageCenter">
+      <Slider {...settings}>
+        {messageCenterData.map((item, idx) => (
+          <div className="messageBlock">
+            {item.data.map((data) => {
+              return (
+                <li>
+                  <div className="tag">{data.tag}</div>
+                  <div className="date">{data.date}</div>
+                  <div className="title">{data.title}</div>
+                </li>
+              );
+            })}
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
 
-export default MessageCenter
+export default MessageCenter;
