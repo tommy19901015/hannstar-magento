497e1a5b75160111e3908b1bc9960e61ed5ce894
unknown <tommylee@hannstar.com>
2022-09-23 17:46:45 +0800

    add form page

--- src/pages/partner-fcp-product_fcp_product_list/main.tsx
+++ src/pages/partner-fcp-product_fcp_product_list/main.tsx
@@ -5,10 +5,14 @@ import { ColType } from "../../component/columns/interface";
 import Form from "../../component/form/main";
 import Breadcrumbs from "../../component/breadcrumbs/main";
 import { I_Props, FormType } from "../../component/form/interface";
+import ReactPaginate from 'react-paginate';
+import testTableData from "./testTableData.json"
 import "./css.scss";
 
 const PartnerFcpProductFcpProductList: React.FC = () => {
   const pageName = "partner-fcp-product_fcp_product_list";
+
+  const fakeData: any = testTableData
 
   const breadcrumbsData = {
     title: "",
@@ -104,17 +108,40 @@ const PartnerFcpProductFcpProductList: React.FC = () => {
     </div>)
   }
 
-  const FormContent = () => {
+  const FilterStateBlock = () => {
+    return (<div className="filterStateBlock">
+      <div>所有產品</div>
+      <div>審核中</div>
+      <div>未通過審核</div>
+      <div>草稿區</div>
+    </div>)
+  }
+
+  const FormTopBlock = () => {
     return (
-      <div>
-        <div>產品清單</div>
-        <div>
+      <div className="formTopBlock">
+        <div className="title">產品清單</div>
+        <div className="searchBlock">
           <input type="text" placeholder="Search" />
           <div className="searchBtn">查詢</div>
         </div>
 
       </div>
     )
+  }
+
+  interface I_Table {
+    state: string
+    productNo: string
+    HSDNo: string
+    size: string
+    resolution: string
+    auditState: string
+    detailSpecs: string
+    productPhoto: string
+    productData: string
+    fontState: string
+    date: string
   }
 
   const ContentBlock = () => {
@@ -125,10 +152,95 @@ const PartnerFcpProductFcpProductList: React.FC = () => {
           <ProductCountInfoBlock {...productCountInfoData} />
         </div>
         <ProductToolBar />
+        <FormTopBlock />
+        <FilterStateBlock />
+        <PaginatedForm itemsPerPage={10} />
       </div>
     )
   }
 
+  //-------------------------------------------
+
+  const FormBlock = ({ currentItems }: any) => {
+    return (
+      <table className="productListTable">
+        <thead>
+          <tr>
+            <td>外包型號</td>
+            <td>HSD型號</td>
+            <td>吋別</td>
+            <td>解析度</td>
+            <td>審核狀態</td>
+            <td>詳細規格</td>
+            <td>產品照片</td>
+            <td>產品資料</td>
+            <td>前台狀態</td>
+            <td>建立日期</td>
+          </tr>
+        </thead>
+        <tbody>
+          {currentItems && currentItems.map((item: any) => (
+            <tr>
+              <td>{item.productNo}</td>
+              <td>{item.HSDNo}</td>
+              <td>{item.size}</td>
+              <td>{item.resolution}</td>
+              <td>{item.auditState}</td>
+              <td>{item.detailSpecs}</td>
+              <td>{item.productPhoto}</td>
+              <td>{item.productData}</td>
+              <td>{item.fontState}</td>
+              <td>{item.date}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+
+  const PaginatedForm = ({ itemsPerPage }: any) => {
+    const [currentItems, setCurrentItems] = useState(null);
+    const [pageCount, setPageCount] = useState(0);
+    const [itemOffset, setItemOffset] = useState(0);
+
+    useEffect(() => {
+      const endOffset = itemOffset + itemsPerPage;
+      console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+      setCurrentItems(fakeData.slice(itemOffset, endOffset));
+      setPageCount(Math.ceil(fakeData.length / itemsPerPage));
+    }, [itemOffset, itemsPerPage]);
+
+    const handlePageClick = (event: any) => {
+      const newOffset = (event.selected * itemsPerPage) % fakeData.length;
+      console.log(
+        `User requested page number ${event.selected}, which is offset ${newOffset}`
+      );
+      setItemOffset(newOffset);
+    };
+
+    return (
+      <div className="paginatedForm">
+        <FormBlock currentItems={currentItems} />
+        <ReactPaginate
+          nextLabel=""
+          onPageChange={handlePageClick}
+          pageCount={pageCount}
+          previousLabel=""
+          pageClassName="page-item"
+          pageLinkClassName="page-link"
+          previousClassName="pagePrevious"
+          previousLinkClassName="page-link"
+          nextClassName="pageNext"
+          nextLinkClassName="page-link"
+          breakLabel="..."
+          breakClassName="page-item"
+          breakLinkClassName="page-link"
+          containerClassName="pagination"
+          activeClassName="active"
+        />
+      </div>
+    );
+  }
 
 
   return (
@@ -138,8 +250,10 @@ const PartnerFcpProductFcpProductList: React.FC = () => {
         type={ColType.OneCol}
         content={<ContentBlock />}
       />
+
+
     </Layout>
   );
-};
+}
 
 export default PartnerFcpProductFcpProductList;
