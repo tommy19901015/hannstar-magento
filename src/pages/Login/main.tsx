f0c80f5ae5319c1644fcf1000abac20f385a9e91
李明軒 <tommy19901015@gmail.com>
2022-10-24 15:06:06 +0800

    login page

--- src/pages/Login/main.tsx
+++ src/pages/Login/main.tsx
@@ -1,126 +1,127 @@
 import React, { useState, useEffect, useRef } from "react";
+import Layout from "../../component/layout/main";
 import AccountTemplate from "../../templates/AccountTemplate/main"
 import "./css.scss";
 
 const HannstarLogin: React.FC = () => {
   const pageName = "HannstarLogin";
-  const [account, setAccount] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [errorMessage, setErrorMessage] = useState<any>("");
 
-  const loginBlock: any = useRef();
-  const errorMessageBlock: any = useRef();
+  const LoginContent = () => {
+    const [account, setAccount] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [errorMessage, setErrorMessage] = useState<any>("");
 
-  useEffect(() => {
-    const magentoDom: any = document.getElementById("hannstar-magento-login");
-    if (magentoDom) loginBlock.current.appendChild(magentoDom);
+    const loginBlock: any = useRef();
+    const errorMessageBlock: any = useRef();
 
-    const magentoErrorMessageDom: any =
-      document.getElementsByClassName("page messages")[0];
-    if (magentoErrorMessageDom)
-      errorMessageBlock.current.appendChild(magentoErrorMessageDom);
+    useEffect(() => {
+      const magentoDom: any = document.getElementById("hannstar-magento-login");
+      if (magentoDom) loginBlock.current.appendChild(magentoDom);
 
-    setAccount(getMagentoAccount().value);
-    setPassword(getMagentoPassword().value);
-  }, []);
+      const magentoErrorMessageDom: any =
+        document.getElementsByClassName("page messages")[0];
+      if (magentoErrorMessageDom)
+        errorMessageBlock.current.appendChild(magentoErrorMessageDom);
 
-  const getMagentoAccount = () => {
-    const emailDom: any = document.getElementById("email");
-    return emailDom ? emailDom : "";
-  };
+      setAccount(getMagentoAccount().value);
+      setPassword(getMagentoPassword().value);
+    }, []);
 
-  const getMagentoPassword = () => {
-    const passwordDom: any = document.getElementById("pass");
-    return passwordDom ? passwordDom : "";
-  };
+    const getMagentoAccount = () => {
+      const emailDom: any = document.getElementById("email");
+      return emailDom ? emailDom : "";
+    };
 
-  const handleAccout = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (getMagentoAccount()) {
-      getMagentoAccount().value = e.target.value;
-    }
+    const getMagentoPassword = () => {
+      const passwordDom: any = document.getElementById("pass");
+      return passwordDom ? passwordDom : "";
+    };
 
-    setAccount(e.target.value);
-  };
+    const handleAccout = (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (getMagentoAccount()) {
+        getMagentoAccount().value = e.target.value;
+      }
 
-  const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (getMagentoPassword()) {
-      getMagentoPassword().value = e.target.value;
-    }
+      setAccount(e.target.value);
+    };
 
-    setPassword(e.target.value);
-  };
+    const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (getMagentoPassword()) {
+        getMagentoPassword().value = e.target.value;
+      }
 
-  const handleLogin = () => {
-    console.log({ account, password });
-    const send2: any = document.getElementById("send2");
-    if (send2) {
-      send2.click();
-    }
-  };
+      setPassword(e.target.value);
+    };
 
-  const LayoutContent = () => {
-    return(
-      <>
-      <div className={`${pageName}`}>
-        <div className="loginBlock">
-          <div ref={errorMessageBlock}></div>
-          <h2>登入</h2>
+    const handleLogin = () => {
+      console.log({ account, password });
+      const send2: any = document.getElementById("send2");
+      if (send2) {
+        send2.click();
+      }
+    };
+    return (
+      <div className="hannstarLoginBlock">
+        <div ref={errorMessageBlock}></div>
+        <h2>登入</h2>
 
-          <div className="columnBlock">
-            <div className="title required">帳號</div>
-            <div className="bodyBlock input">
-              <input
-                type="text"
-                onChange={handleAccout}
-                value={account}
-                placeholder="請填入您的Email"
-              />
-            </div>
+        <div className="columnBlock">
+          <div className="title required">帳號</div>
+          <div className="bodyBlock input">
+            <input
+              type="text"
+              onChange={handleAccout}
+              value={account}
+              placeholder="請填入您的Email"
+            />
           </div>
-
-          <div className="columnBlock">
-            <div className="title required">密碼</div>
-            <div className="bodyBlock input">
-              <input onChange={handlePassword} type="text" value={password} />
-            </div>
-          </div>
-
-          <div className="columnBlock">
-              <div className="bodyBlock select">
-                <div className="hannstarCheckBox spaceBetween alignCenter">
-                  <input
-                    id="checkBox1"
-                    type="checkBox"
-                    value="yes"
-                    name="yes"
-                  />
-                  <label htmlFor="checkBox1">
-                  記住帳號<a href="/">忘記密碼</a>
-                  </label>
-                </div>
-              </div>
-            </div>
-
-          <div className="loginBtn" onClick={handleLogin}>
-            登入
-          </div>
-          <p className="create">
-            還沒有HannStar帳號? <a href="">建立帳號</a>
-          </p>
-
-          <div ref={loginBlock} className="magentoLoginBlock"></div>
         </div>
+
+        <div className="columnBlock">
+          <div className="title required">密碼</div>
+          <div className="bodyBlock input">
+            <input onChange={handlePassword} type="text" value={password} />
+          </div>
+        </div>
+
+        <div className="columnBlock">
+          <div className="bodyBlock select">
+            <div className="hannstarCheckBox spaceBetween alignCenter">
+              <input
+                id="checkBox1"
+                type="checkBox"
+                value="yes"
+                name="yes"
+              />
+              <label htmlFor="checkBox1">
+                記住帳號<a href="/">忘記密碼</a>
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div className="loginBtn" onClick={handleLogin}>
+          登入
+        </div>
+        <p className="create">
+          還沒有HannStar帳號? <a href="">建立帳號</a>
+        </p>
+
+        <div ref={loginBlock} className="magentoLoginBlock"></div>
       </div>
-    </>
     )
   }
-  const partnerFcpTemplateProp = {
-    content: <LayoutContent />,
+  const accountTemplateProp = {
+    content: <LoginContent />,
   };
-  
+
 
   return (
-    <AccountTemplate {...partnerFcpTemplateProp}/>
+    <Layout>
+      <div className={`${pageName}Container`}>
+        <AccountTemplate {...accountTemplateProp} />
+      </div>
+    </Layout>
   );
 };
 
--- src/templates/AccountTemplate/main.tsx
+++ src/templates/AccountTemplate/main.tsx
@@ -5,32 +5,31 @@ import { ColType } from "../../component/columns/interface";
 import { I_AccountTemplate } from "./interface";
 import { initialServiceData } from "./pageData";
 import { serviceType } from "./interface";
+// import earth from "./icon/earth"
 import "./css.scss";
 const AccountTemplate: React.FC<I_AccountTemplate> = ({ content }) => {
   const templateName = "AccountTemplate";
+
   const [serviceData, setServiceData] =
     useState<serviceType[]>(initialServiceData);
 
   const MainContainer = () => {
     return (
-      <Layout>
-      <div className={`${templateName}ContentBlock`}>
-        <Columns
-          type={ColType.TwoColFullPage}
-          widthL={50}
-          widthR={50}
-          contentL={content}
-          contentR={
-            <div className="HannstarLogin">
-            <div className="serviceBlock">
+      <Columns
+        type={ColType.TwoColFullPage}
+        widthL={50}
+        widthR={50}
+        contentL={content}
+        contentR={
+          <div className={`${templateName}ServiceBlock`}>
             <h3>立即註冊翰宇彩晶會員</h3>
             <>
               {serviceData.map((category) => (
                 <div className="content">
                   <h4 className="title">{category.categoryTitle}</h4>
                   <div className="items">
-                    {category.serviceItems.map((item) => (
-                      <div className="item">
+                    {category.serviceItems.map(item => (
+                      <div className={`item block${category.categoryId}`}>
                         <img className="" src={item.icon} alt={item.icon} />
                         <span className="">{item.title}</span>
                         <p className="">{item.directions}</p>
@@ -41,11 +40,8 @@ const AccountTemplate: React.FC<I_AccountTemplate> = ({ content }) => {
               ))}
             </>
           </div>
-          </div>
         }
-        />
-      </div>
-      </Layout>
+      />
     );
   };
   return <MainContainer />;
