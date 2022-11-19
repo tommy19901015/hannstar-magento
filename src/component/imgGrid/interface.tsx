5a444fb961dc814404512dc5a29787baa11e8044
tommy <tommy19901015@gmail.com>
2022-11-19 15:32:02 +0800

    add comp

---
+++ src/component/imgGrid/interface.tsx
@@ -0,0 +1,5 @@
+export interface I_ImgGrid {
+  data: {
+    src: string;
+  }[];
+}
---
+++ src/component/imgGrid/main.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { I_ImgGrid } from "./interface";
+import "./css.scss";
+
+const ImgGrid: React.FC<I_ImgGrid> = ({ data }) => {
+  const componentName = "ImgGrid";
+
+  return (
+    <div className={`${componentName}Content`}>
+      {data.map((item, idx) => {
+        return (
+          <img
+            className={`${componentName}Img`}
+            src={item.src}
+            alt={`${componentName}${idx}`}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default ImgGrid;
