6680c0f805ea650cca4edb08a111414939d9f506
tommy <tommy19901015@gmail.com>
2022-11-20 23:38:45 +0800

    add modify magento code

---
+++ magento/Hannstar/About/Controller/Family/View.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hannstar\About\Controller\Family;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\View\Result\Page;
+
+class View extends Action
+{
+    public function execute()
+    {
+        /** @var Page $resultPage */
+        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+    }
+}
