c3a71d803bac91149f478fc91ec46112c0a7068c
Dora Lee <dora80823@gmail.com>
2022-11-22 12:16:46 +0800

    調整style

--- src/pages/TFT_Index/pageData.tsx
+++ src/pages/TFT_Index/pageData.tsx
@@ -14,19 +14,8 @@ function usePageData() {
       data: [
         {
           src: "https://fakeimg.pl/1000x700/?text=Image",
-          title: "永續管理",
-          text: "追求成長與創新，深耕企業文化與核心價值，攜手利害關係人，透過價值鏈的延伸，實現永續經營的目標。",
-          textAlign: TextAlign.BottomLeft,
-        },
-      ],
-    },
-    {
-      type: "FullBanner",
-      data: [
-        {
-          src: "https://fakeimg.pl/1000x700/?text=Image",
-          title: "永續管理",
-          text: "追求成長與創新，深耕企業文化與核心價值，攜手利害關係人，透過價值鏈的延伸，實現永續經營的目標。",
+          title: "",
+          text: "",
           textAlign: TextAlign.BottomLeft,
         },
       ],
