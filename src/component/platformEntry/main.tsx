ec9f863f75b027ecd928503023091bf2566b7214
李明軒 <tommy19901015@gmail.com>
2022-11-24 17:56:59 +0800

    add data

--- src/component/platformEntry/main.tsx
+++ src/component/platformEntry/main.tsx
@@ -3,16 +3,14 @@ import { I_PlatformEntry } from "./interface";
 import "./css.scss";
 
 const PlatformEntry: React.FC<I_PlatformEntry> = ({ data }) => {
-  console.log("fff", data);
   return (
     <div
-      className={`platformEntryBlock ${
-        data.length < 4 ? "row" : data.length % 2 ? "odd" : ""
-      }`}
+      className={`platformEntryBlock ${data.length < 4 ? "row" : data.length % 2 ? "odd" : ""
+        }`}
     >
       {data.map((item, idx) => (
         <div className={`item style${idx}`} key={idx}>
-          <a href={item.href} target="_blank" rel="noreferrer">
+          <a href={item.href}>
             <img src={item.src} alt={item.title} />
             <div className="platformTitle">{item.title}</div>
           </a>
