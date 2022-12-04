ac07e66f11663194f56fbb2f9d7fe1d52b8d9d2a
Dora Lee <dora80823@gmail.com>
2022-12-04 16:59:39 +0800

    hannspree add img

--- src/pages/Hannspree_Custom/pageData.tsx
+++ src/pages/Hannspree_Custom/pageData.tsx
@@ -3,7 +3,7 @@ import { TextAlign , BannerType} from "../../component/banner/interface";
 import { GraphicsTheme } from "../../component/graphicsCard/interface";
 import { I_TemplateLayout } from "../../templates/TemplateLayout/interface";
 import { TabEnum } from "../../types/Tmeplate1/interface";
-
+import { urlConfig } from "../../config/urlSetting";
 
 function usePageData() {
     const { t } = useTranslation();
@@ -13,7 +13,7 @@ function usePageData() {
                 type: "FullBanner",
                 data: [
                   {
-                    src: "https://fakeimg.pl/1000x700/?text=Image",
+                    src: `${urlConfig().s3Url}/Image/hannspree/customization/img_customization_banner.jpg`,
                     title: "",
                     text: "",
                     textAlign: TextAlign.BottomLeft,
@@ -30,7 +30,7 @@ function usePageData() {
                     content: "瀚斯寶麗除了顯示紙整機標準品之外，更提供了產品客製化服務。在穿戴產品、平板或筆記型電腦系列，我們提供了四種客製化方案，提供您做選擇。讓瀚斯寶麗與您一起打造專屬於您的夢幻逸品。"
                   },
                   banner: {
-                    src: "https://fakeimg.pl/1000x700/?text=Image",
+                    src: `${urlConfig().s3Url}/Image/hannspree/customization/img_flow.jpg`,
                     title: "",
                     text: "",
                     textAlign: TextAlign.BottomLeft,
@@ -42,7 +42,7 @@ function usePageData() {
               type: "FullBanner",
               data: [
                 {
-                  src: "https://fakeimg.pl/1000x700/?text=Image",
+                  src: `${urlConfig().s3Url}/Image/hannspree/customization/img_prod_category.jpg`,
                   title: "產品類別說明",
                   text: "客製化方案產品類別包含：穿戴式產品、平板與筆記型電腦系列。",
                   textAlign: TextAlign.BottomLeft,
@@ -66,7 +66,7 @@ function usePageData() {
                 theme: GraphicsTheme.Theme1,
                 data: [
                   {
-                    src:"https://fakeimg.pl/100x100/?text=Image",
+                    src:`${urlConfig().s3Url}/Image/hannspree/customization/img_plan_appearance.png`,
                     title:"品牌識別",
                     text:[
                       "．企業Logo絹印",
@@ -77,7 +77,7 @@ function usePageData() {
                     btnText:"",
                   },
                   {
-                    src:"https://fakeimg.pl/100x100/?text=Image",
+                    src:`${urlConfig().s3Url}/Image/hannspree/customization/img_exterior.png`,
                     title:"外型設計",
                     text:[
                       "．整機外型設計",
@@ -87,7 +87,7 @@ function usePageData() {
                     btnText:"",
                   },
                   {
-                    src:"https://fakeimg.pl/100x100/?text=Image",
+                    src:`${urlConfig().s3Url}/Image/hannspree/customization/img_plan_software.png`,
                     title:"軟體訂製",
                     text:[
                       "．整機內建功能規劃",
@@ -99,7 +99,7 @@ function usePageData() {
                     btnText:"",
                   },
                   {
-                    src:"https://fakeimg.pl/100x100/?text=Image",
+                    src:`${urlConfig().s3Url}/Image/hannspree/customization/img_plan_all.png`,
                     title:"全客製化服務",
                     text:[
                       "完整客製您的產品，可包含以下項目：",
@@ -115,6 +115,19 @@ function usePageData() {
                 ],
               }
             ]
+          },
+          {
+            type: "SustainabilityGraphics3",
+            data: [
+              {
+                src: `${urlConfig().s3Url}/Image/hannspree/solution/img_contact_contactus.png`,
+                title: "與我們聯繫",
+                btnText: "聯絡我們",
+                btnHref: "/",
+                content:
+                  "如您有品牌合作或是任何客製化的需求，請點選以下聯絡我們，填寫表單資料後，將由業務同仁與您聯繫。",
+              },
+            ],
           },
         ]
     }
--- src/pages/Hannspree_Iindex/pageData.tsx
+++ src/pages/Hannspree_Iindex/pageData.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from "react-i18next";
 import { TextAlign , BannerType} from "../../component/banner/interface";
 import { GraphicsTheme } from "../../component/graphicsCard/interface";
+import { urlConfig } from "../../config/urlSetting";
 import { I_TemplateLayout } from "../../templates/TemplateLayout/interface";
 import { TabEnum } from "../../types/Tmeplate1/interface";
 
@@ -12,7 +13,7 @@ function usePageData() {
         type: "FullBanner",
         data: [
           {
-            src: "https://fakeimg.pl/1000x700/?text=Image",
+            src: `${urlConfig().s3Url}/Image/hannspree/index/img_main_banner.png`,
             title: "",
             text: "",
             textAlign: TextAlign.BottomLeft,
@@ -24,17 +25,17 @@ function usePageData() {
         type: "MessageCenterPlatform",
         data: [
           {
-            src: "https://webdev.hannstar.com/upload/ad_icon_list/twL_ad_21G07_t4gtmsjux5.png",
+            src: `${urlConfig().s3Url}/Image/hannspree/index/icon_menu_product.png`,
             title: "Product",
             href: "/",
           },
           {
-            src: "https://webdev.hannstar.com/upload/ad_icon_list/twL_ad_21G07_xnf94vwkwm.png",
+            src: `${urlConfig().s3Url}/Image/hannspree/index/icon_menu_solution.png`,
             title: "Solutions",
             href: "/",
           },
           {
-            src: "https://webdev.hannstar.com/upload/ad_icon_list/twL_ad_21G07_pub4mi8vcn.png",
+            src:`${urlConfig().s3Url}/Image/hannspree/index/icon_menu_customization.png`,
             title: "Customization Design",
             href: "/",
           },
@@ -54,13 +55,13 @@ function usePageData() {
         type: "SustainabilityGraphics3",
         data: [
           {
-            src: "https://fakeimg.pl/1000x700/?text=Image",
+            src: `${urlConfig().s3Url}/Image/hannspree/index/img_intro_product.png`,
             title: "顯示紙整機產品",
             content:
               "以Paper Display顯示紙為面板技術核心，設計手錶、平板與筆記型電腦等系列產品。強調陽光下可視、護眼、低功耗、多媒體播放等特色，讓你不論在戶外運動、教學場域都可以盡情使用。",
           },
           {
-            src: "https://fakeimg.pl/1000x700/?text=Image",
+            src: `${urlConfig().s3Url}/Image/hannspree/index/img_intro_customization.png`,
             title: "客製化服務",
             content:
               "不只品牌產品，更依照客戶需求，進行產品客製化服務。我們擁有完整的設計、研發與專案管理團隊，從概念到成品，打造您的客製化整機產品。",
@@ -84,21 +85,21 @@ function usePageData() {
             theme: GraphicsTheme.Theme1,
             data: [
               {
-                src: "https://fakeimg.pl/100x100/?text=Image",
+                src: `${urlConfig().s3Url}/Image/hannspree/index/img_service_product.png`,
                 title: "整機產品",
                 text: "顯示紙整機產品系列，包含：穿戴式裝置、平板、筆電與顯示器。",
                 href: "/",
                 btnText: "了解更多",
               },
               {
-                src: "https://fakeimg.pl/100x100/?text=Image",
+                src: `${urlConfig().s3Url}/Image/hannspree/index/img_service_customization.png`,
                 title: "客製化服務",
                 text: "針對客戶需求，提供多種方案，進行產品客製。",
                 href: "/",
                 btnText: "了解更多",
               },
               {
-                src: "https://fakeimg.pl/100x100/?text=Image",
+                src: `${urlConfig().s3Url}/Image/hannspree/index/img_service_solution.png`,
                 title: "解決方案",
                 text: "搭配顯示紙產品推出教育與健康照護的解決方案。",
                 href: "/",
