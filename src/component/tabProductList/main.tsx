8c1e13823275bc3402966a97f78485f7364a8e40
Dora Lee <dora80823@gmail.com>
2022-12-12 16:13:22 +0800

    modify data ang page

--- src/component/tabProductList/main.tsx
+++ src/component/tabProductList/main.tsx
@@ -2,147 +2,30 @@ import React, { useState, useEffect } from "react";
 import "./css.scss";
 import { I_TabsModule, I_TabDataModule } from "./interface";
 
-const productListData = [
-  {
-    tabName: "11111",
-    tabData: [
-      {
-        src: "https://fakeimg.pl/350x200/?text=Hello",
-        title: "1111titletitle",
-        isHot: true,
-        content:
-          "contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent",
-        productId: "1",
-      },
-      {
-        src: "https://fakeimg.pl/350x200/?text=Hello",
-        title: "titletitle",
-        content:
-          "contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent",
-        productId: "1",
-      },
-      {
-        src: "https://fakeimg.pl/350x200/?text=Hello",
-        title: "titletitle",
-        content:
-          "contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent",
-        productId: "1",
-      },
-      {
-        src: "https://fakeimg.pl/350x200/?text=Hello",
-        title: "titletitle",
-        content:
-          "contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent",
-        productId: "1",
-      },
-    ],
-  },
-  {
-    tabName: "22222",
-    tabData: [
-      {
-        src: "https://fakeimg.pl/350x200/?text=Hello",
-        title: "22222titletitle",
-        content:
-          "contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent",
-        productId: "1",
-      },
-      {
-        src: "https://fakeimg.pl/350x200/?text=Hello",
-        title: "titletitle",
-        content:
-          "contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent",
-        productId: "1",
-      },
-      {
-        src: "https://fakeimg.pl/350x200/?text=Hello",
-        title: "titletitle",
-        content:
-          "contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent",
-        productId: "1",
-      },
-      {
-        src: "https://fakeimg.pl/350x200/?text=Hello",
-        title: "titletitle",
-        content:
-          "contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent",
-        productId: "1",
-      },
-    ],
-  },
-  {
-    tabName: "33333",
-    tabData: [
-      {
-        src: "https://fakeimg.pl/350x200/?text=Hello",
-        title: "33333titletitle",
-        content:
-          "contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent",
-        productId: "1",
-      },
-      {
-        src: "https://fakeimg.pl/350x200/?text=Hello",
-        title: "titletitle",
-        content:
-          "contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent",
-        productId: "1",
-      },
-      {
-        src: "https://fakeimg.pl/350x200/?text=Hello",
-        title: "titletitle",
-        content:
-          "contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent",
-        productId: "1",
-      },
-      {
-        src: "https://fakeimg.pl/350x200/?text=Hello",
-        title: "titletitle",
-        content:
-          "contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent",
-        productId: "1",
-      },
-    ],
-  },
-  {
-    tabName: "44444",
-    tabData: [
-      {
-        src: "https://fakeimg.pl/350x200/?text=Hello",
-        title: "titletitle",
-        content:
-          "contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent",
-        productId: "1",
-      },
-      {
-        src: "https://fakeimg.pl/350x200/?text=Hello",
-        title: "titletitle",
-        content:
-          "contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent",
-        productId: "1",
-      },
-      {
-        src: "https://fakeimg.pl/350x200/?text=Hello",
-        title: "titletitle",
-        content:
-          "contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent",
-        productId: "1",
-      },
-      {
-        src: "https://fakeimg.pl/350x200/?text=Hello",
-        title: "titletitle",
-        content:
-          "contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent",
-        productId: "1",
-      },
-    ],
-  },
-];
+const ProductCard: React.FC<any> = ({ src, title, content, btnTextL, btnTextR },idx) => {
+  return (
+    <div className="productBlock" key={idx}>
+      <div className="imgBlock">
+        <img src={src} alt={title} />
+      </div>
+      <div className="contentWrap">
+        <div className="productTitle">{title}</div>
+        <div className="productContent">{content}</div>
+        <div className="btnBlock">
+          <div className="btn1">{btnTextL}</div>
+          <div className="btn2">{btnTextR}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
 
 const TabProductList: React.FC<I_TabsModule[]> = (data) => {
   const componentName = "TabProductList";
   const [initTabList, setInitTabList,] = useState<I_TabsModule[]>([]);
-  const [productListData, setProductListData] = useState<I_TabDataModule[]>([]);
-  const [tab, setTab] = useState<number>(0);
+  const [activeTab, setActiveTab] = useState<number>(0);
 
   useEffect(() => {
     let initTab = Object.values(data);
@@ -151,18 +34,14 @@ const TabProductList: React.FC<I_TabsModule[]> = (data) => {
 
   const TabBlock = () => {
     const changeTab = (tabName: string, index: number) => {
-      setTab(index);
-      const filterData = tabName
-        ? initTabList.filter(item => item.tabName === tabName)
-        : initTabList;
-      setProductListData(filterData[0].tabData);
+      setActiveTab(index);
     };
 
     return (
       <div className="tabContent">
         {initTabList.map((item, index) => (
           <div
-            className={`tab ${tab === index ? "active" : ""}`}
+            className={`tab ${activeTab === index ? "active" : ""}`}
             onClick={() => changeTab(item.tabName, index)}
           >
             {item.tabName}
@@ -171,31 +50,19 @@ const TabProductList: React.FC<I_TabsModule[]> = (data) => {
       </div>
     );
   };
-
-  const ProductCard: React.FC<any> = ({ title, src, content, index, btnTextL, btnTextR }) => {
-    return (
-      <div className="productBlock" key={index}>
-        <div className="imgBlock">
-          <img src={src} alt={title} />
-        </div>
-        <div className="contentWrap">
-          <div className="productTitle">{title}</div>
-          <div className="productContent">{content}</div>
-          <div className="btnBlock">
-            <div className="btn1">{btnTextL}</div>
-            <div className="btn2">{btnTextR}</div>
-          </div>
-        </div>
-      </div>
-    );
-  };
-
   return (
+    
     <div className={`${componentName}Content`}>
       <TabBlock />
       <div className="productListBlock">
-        {productListData.map((item, index) => (
-          <ProductCard {...{ ...item, index }} />
+        {initTabList.map((tab, idx) => (
+          idx === activeTab && (
+            <>
+              {tab.tabData.map((child, childIdx) => (
+                <ProductCard {...child} idx={childIdx} />
+              ))}
+            </>
+          )
         ))}
       </div>
     </div>
--- src/pages/PaperDisplay_Index/pageData.tsx
+++ src/pages/PaperDisplay_Index/pageData.tsx
@@ -3,6 +3,7 @@ import { TextAlign , BannerType} from "../../component/banner/interface";
 import { GraphicsTheme } from "../../component/graphicsCard/interface";
 import { I_TemplateLayout } from "../../templates/TemplateLayout/interface";
 import { TabEnum } from "../../types/Tmeplate1/interface";
+import { urlConfig } from "../../config/urlSetting";
 
 function usePageData() {
   const { t } = useTranslation();
@@ -12,7 +13,7 @@ function usePageData() {
         type: "FullBanner",
         data: [
           {
-            src: "https://fakeimg.pl/1000x700/?text=Image",
+            src:  `${urlConfig().s3Url}/Image/paperdisplay/index/topbanner.jpg`,
             title: "顯示紙",
             text: "Paper Display",
             textAlign: TextAlign.BottomLeft,
@@ -24,17 +25,17 @@ function usePageData() {
         type: "MessageCenterPlatform",
         data: [
           {
-            src: "https://webdev.hannstar.com/upload/ad_icon_list/twL_ad_21G07_t4gtmsjux5.png",
+            src: `${urlConfig().s3Url}/Image/paperdisplay/index/Technologyicon.png`,
             title: "顯示紙技術",
             href: "/",
           },
           {
-            src: "https://webdev.hannstar.com/upload/ad_icon_list/twL_ad_21G07_xnf94vwkwm.png",
+            src: `${urlConfig().s3Url}/Image/paperdisplay/index/Application_icon.png`,
             title: "顯示紙應用",
             href: "/",
           },
           {
-            src: "https://webdev.hannstar.com/upload/ad_icon_list/twL_ad_21G07_pub4mi8vcn.png",
+            src: `${urlConfig().s3Url}/Image/paperdisplay/index/Greensupplychainicon.png`,
             title: "綠色供應鏈",
             href: "/",
           },
@@ -46,7 +47,13 @@ function usePageData() {
           {
             title: "關於顯示紙",
             content:
-              "瀚宇彩晶為保持企業永續競爭力及秉持環境保護願景，全新開發兼具創新和環境友善的顯示紙產品。顯示紙採用高反射率金屬材料，可達到戶外強光下清晰可視; 其無背光模組設計，可大幅降低功耗（節省約80~90%）。另運用CNP(Crystal Nano Pixel) 與 STP (Soft Texture Paper) 技術，讓顯示紙的閱讀、書寫有如真實紙張的體驗感。此外，顯示紙也可支持全彩 (16.7百萬色) 、影片播放、寬溫顯示 (-40℃~85℃)、無藍光護眼特性，提供優質影像品質，豐富視覺饗宴。顯示紙除產品設計上較傳統顯示器更省電節能，在材料、製程、物流包材上，也致力於減少產品碳足跡與實踐永續理念，協助客戶打造更環保節能的產品，一同共創綠色未來。",
+              `永續發展與淨零碳排已成為全球共同追求的目標，瀚宇彩晶秉持保護環境守護綠色地球為理念，全新開發環境友善的創新「顯示紙」產品。
+
+              「顯示紙」採用 A.B.T (Argentum Birefringent Technology) 顯示技術，在戶外強光下畫面依然清晰可視，且可達到 16.7M 自然全彩無間斷動態顯示。在正常環境光源下，搭配自主開發的超低功耗 IC，與傳統顯示器最高可節省約 80-90% 功耗，達到節能省電並大幅度降低碳排放問題。另運用 C.N.P (Crystal Nano Pixel) 與 S.T.P (Soft Texture Paper) 技術，讓「顯示紙」的閱讀、書寫有如真實紙張的體驗感，同步讓眼睛可減少負擔降低傷害。
+              
+              「顯示紙」除產品設計上較傳統顯示器更省電節能，在材料、製程、物流包材上，也致力於減少產品碳足跡與實踐永續理念，協助客戶打造更環保節能的產品，一同共創綠色未來。
+              
+              `,
           },
         ],
       },
@@ -54,9 +61,10 @@ function usePageData() {
         type: "TitleContentBlock",
         data: [
           {
-            title: "顯示紙特性",
+            title: "顯示紙創新技術研發 啟動智慧城市節能減碳新時代",
             content:
-              "顯示紙以環境永續之節能設計概念出發，具備戶外可視清晰護眼、超低功耗強韌續航、寬溫設計征服極境等顯示特性，於戶外強光和極端溫度下仍維持清晰可讀、正常顯示 ; 另兼具貼近真實紙感書寫與繪畫體驗。",
+              `智慧城市是透過數位化結合 5G、物聯網等技術達到數據傳遞，經由大數據分析而達到智慧化。智慧城市的發展，智慧裝置溝通媒介主要透過顯示屏幕，如何選擇節能減碳創新顯示產品，顯示紙產品分為六大應用領域從產品使用情境出發，以循環經濟理念建構節能減碳智慧城市創新應用，開創綠色創新經濟新價值。
+              `,
           },
         ],
       },
@@ -64,9 +72,9 @@ function usePageData() {
         type: "BannerBlock",
         data: [
           {
-            src: "https://fakeimg.pl/1000x700/?text=Image",
-            title: "節能設計環境永續",
-            text: "綠色供應商、綠色產品設計、綠色製程、環保物流與包材。",
+            src: `${urlConfig().s3Url}/Image/paperdisplay/index/banner_smarttransportation.jpg`,
+            title: "交通運輸智慧移動",
+            text: `整合顯示紙導入城際公路與都市道路管理系統，即時資訊運輸資料傳遞進而增進運輸系統的安全，效率及舒適性，以顯示科技實踐環境永續與綠色運輸。`,
             textAlign: TextAlign.BottomLeft,
           },
         ],
@@ -75,9 +83,10 @@ function usePageData() {
         type: "BannerBlock",
         data: [
           {
-            src: "https://fakeimg.pl/1000x700/?text=Image",
-            title: "戶外可視清晰護眼",
-            text: "顯示紙利用環境光反射方式實現畫面顯示",
+            src: `${urlConfig().s3Url}/Image/paperdisplay/index/banner_smart_building.jpg`,
+            title: "智慧建築環境打造",
+            text: `顯示紙在建築聯網應用中更添色彩，提供未來建築的便利與安全選擇。協助智慧建築相關產品於不斷更新的應用裡顯示出多種可能性，善盡企業友善環境責任。
+            `,
             textAlign: TextAlign.BottomLeft,
           },
         ],
@@ -86,9 +95,10 @@ function usePageData() {
         type: "BannerBlock",
         data: [
           {
-            src: "https://fakeimg.pl/1000x700/?text=Image",
-            title: "超低功耗強韌續航",
-            text: "顯示紙無背光、低刷新率設計，達到省電。",
+            src: `${urlConfig().s3Url}/Image/paperdisplay/index/banner_smartretailing.jpg`,
+            title: "智慧零售創新商模",
+            text: `透過低碳顯示紙聯網應用，協助傳統的產品銷售數位轉型，整合數據效益與服務模式，為消費者創造即時、高互動的新興體驗。
+            `,
             textAlign: TextAlign.BottomLeft,
           },
         ],
@@ -97,9 +107,9 @@ function usePageData() {
         type: "BannerBlock",
         data: [
           {
-            src: "https://fakeimg.pl/1000x700/?text=Image",
-            title: "真實紙感體驗",
-            text: "透過獨家Soft Texture Paper(STP)技術，打造貼近真實紙張體驗感。",
+            src: `${urlConfig().s3Url}/Image/paperdisplay/index/banner_smartedutainment.jpg`,
+            title: "智慧育樂沉浸體驗",
+            text: `以顯示紙多螢幕多裝置設備為人機互動介面，延展虛擬空間資訊即時反饋學習成效，圖像教材高彩度擬真效果完美呈現，提昇沉浸感與參與感。`,
             textAlign: TextAlign.BottomLeft,
           },
         ],
@@ -108,76 +118,27 @@ function usePageData() {
         type: "BannerBlock",
         data: [
           {
-            src: "https://fakeimg.pl/1000x700/?text=Image",
-            title: "寬溫設計征服極境",
-            text: "顯示紙在極端溫度（-30℃~80℃）下，仍維持正常顯示。",
+            src: `${urlConfig().s3Url}/Image/paperdisplay/index/banner_healthcare.jpg`,
+            title: "健康關懷健康生活",
+            text: `運動健康及智慧醫療等關懷物聯應用下，顯示紙系統打造下個世代健康關懷新世界。運動健康、智慧醫療等數據一目了然、多元豐富與便捷的溝通體驗。
+            `,
+            textAlign: TextAlign.BottomLeft,
+          },
+        ],
+      },
+      {
+        type: "BannerBlock",
+        data: [
+          {
+            src: `${urlConfig().s3Url}/Image/paperdisplay/index/banner_IIoT.jpg`,
+            title: "智能製造高效流程",
+            text: `人機介面為物聯相關重要設備，透過顯示紙物聯產品，除可達到強固耐用外，更可提升裝置在室內外產品性能如低功耗、散熱、續航力等為，為智慧製造提升市場競爭力。
+            `,
             textAlign: TextAlign.BottomLeft,
           },
         ],
       },
 
-      {
-        type: "TitleContentBlock",
-        data: [
-          {
-            title: "產品應用",
-            content: "",
-          },
-        ],
-      },
-      {
-        type: "TitleContentGraphicsCard",
-        data: [
-          {
-            rowCount: 3,
-            theme: GraphicsTheme.Theme1,
-            data: [
-              {
-                src: "https://fakeimg.pl/100x100/?text=Image",
-                title: "智慧零售",
-                text: "",
-                href: "/",
-                btnText: "了解更多",
-              },
-              {
-                src: "https://fakeimg.pl/100x100/?text=Image",
-                title: "智慧育樂",
-                text: "透過平台簡易、便利的服務，讓客戶享受更快速的各項，由全方位的售後服務團隊為項勞。",
-                href: "/",
-                btnText: "了解更多",
-              },
-              {
-                src: "https://fakeimg.pl/100x100/?text=Image",
-                title: "交通運輸",
-                text: "透過平台簡易、便利的服務，讓客戶享受更快速的各項，由全方位的售後服務團隊為項勞。",
-                href: "/",
-                btnText: "了解更多",
-              },
-              {
-                src: "https://fakeimg.pl/100x100/?text=Image",
-                title: "智能建築",
-                text: "透過平台簡易、便利的服務，讓客戶享受更快速的各項，由全方位的售後服務團隊為項勞。",
-                href: "/",
-                btnText: "了解更多",
-              },
-              {
-                src: "https://fakeimg.pl/100x100/?text=Image",
-                title: "健康關懷",
-                text: "透過平台簡易、便利的服務，讓客戶享受更快速的各項，由全方位的售後服務團隊為項勞。",
-                href: "/",
-                btnText: "了解更多",
-              },
-              {
-                src: "https://fakeimg.pl/100x100/?text=Image",
-                title: "智能製造",
-                text: "透過平台簡易、便利的服務，讓客戶享受更快速的各項，由全方位的售後服務團隊為項勞。",
-                href: "/",
-                btnText: "了解更多",
-              },
-            ],
-          },
-        ],
-      },
       {
         type: "TitleContentBlock",
         data: [
@@ -195,25 +156,25 @@ function usePageData() {
             theme: GraphicsTheme.Theme1,
             data: [
               {
-                src: "https://fakeimg.pl/100x100/?text=Image",
+                src: `${urlConfig().s3Url}/Image/paperdisplay/index/productinquiry.jpg`,
                 title: "產品查詢",
-                text: "結合平台大數據運行，提供多元化產品，以符合客戶需求",
-                href: "",
-                btnText: "",
+                text: `結合平台大數據運行，提供多元化產品，以符合客戶需求`,
+                href: "/",
+                btnText: "了解更多",
               },
               {
-                src: "https://fakeimg.pl/100x100/?text=Image",
+                src: `${urlConfig().s3Url}/Image/paperdisplay/index/solutionintegration.jpg`,
                 title: "客製化服務",
-                text: "提供客戶專案報價服務",
-                href: "",
-                btnText: "",
+                text: `提供客戶專案報價服務`,
+                href: "/",
+                btnText: "了解更多",
               },
               {
-                src: "https://fakeimg.pl/100x100/?text=Image",
+                src: `${urlConfig().s3Url}/Image/paperdisplay/index/customizationservice.jpg`,
                 title: "方案整合",
-                text: "一站式方案整合服務，提供嵌入式主板、顯示器與UI設計服務",
-                href: "",
-                btnText: "",
+                text: `一站式方案整合服務，提供嵌入式主板、顯示器與UI設計服務`,
+                href: "/",
+                btnText: "了解更多",
               },
             ],
           },
